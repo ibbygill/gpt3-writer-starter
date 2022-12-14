685d2bad404f90b7926428b530c1d749e2fbed9d
Ibjyot Gill <igill47@my.bcit.ca>
2022-12-13 23:43:45 -0800

    including two seperate user inputs for artist and genre

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,6 +9,27 @@ const basePromptPrefix =
 `
 Write me a well-written song that is includes strong lyrics, solid song structure, emotion, and a unique hook that will grab the audience and be written in a style that Drake writes in.
 `
+
+const secondPrompt = 
+  `
+  Take the name below and using that ar
+
+  Title: ${req.body.userInput}
+
+  Table of Contents: ${basePromptOutput.text}
+
+  Lryics
+  `
+
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
+  });
+
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
@@ -16,12 +37,12 @@ const generateAction = async (req, res) => {
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-    temperature: 0.7,
+    temperature: 0.8,
     max_tokens: 500,
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
-
+  
   res.status(200).json({ output: basePromptOutput });
 };
 
